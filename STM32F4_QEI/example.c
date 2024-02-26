0f21b3322a2870da6e9ab5ebf59317ab28627f51
ImadUddien9543 <113409331+ImadUddien9543@users.noreply.github.com>
2024-02-27 05:48:08 +0700

    Update example.c

--- STM32F4_QEI/example.c
+++ STM32F4_QEI/example.c
@@ -16,7 +16,7 @@ float EncRPM, Degree;
 int main(void)
 {
   /* USER CODE BEGIN 2 */
-  Encoder1 = Init_QEI(CH_A_GPIO_Port, CH_B_GPIO_Port, CH_A_Pin, CH_B_Pin, 360, QEI_RPM, QEI_DEG);
+  Encoder1 = Init_QEI(CH_A_GPIO_Port, CH_B_GPIO_Port, CH_A_Pin, CH_B_Pin, 360);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -26,16 +26,17 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  Encoder1->GetANGLE(Encoder1);
-	  Encoder1->GetROT_VEL(Encoder1, 100);
-	  Encoder1->GetLIN_VEL(Encoder1, 100, 10);
-	  Encoder1->GetDIST(Encoder1, 10);
+	  float wheel_r = 10;
+	  uint32_t sample_time = 100;
+	  Encoder1->GetDEG(Encoder1);
+	  Encoder1->GetRPM(Encoder1, sample_time);
+	  Encoder1->GetMTR_S(Encoder1, sample_time, wheel_r);
+	  Encoder1->GetMTR(Encoder1, wheel_r);
 
 	  Degree = Encoder1->DEG;
 	  EncRPM = Encoder1->RPM;
 	  LinDistance = Encoder1->METER;
 	  LinVel = Encoder1->MTR_S;
-
   }
   /* USER CODE END 3 */
 }
