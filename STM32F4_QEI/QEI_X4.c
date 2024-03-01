4b4dae93217e74a73e71a141ca7fd43f9ce33eab
ImadUddien9543 <113409331+ImadUddien9543@users.noreply.github.com>
2024-03-02 04:15:48 +0700

    Update QEI_X4.c

--- STM32F4_QEI/QEI_X4.c
+++ STM32F4_QEI/QEI_X4.c
@@ -1,5 +1,5 @@
 /*
- * QEI_X4.h
+ * QEI.h
  *
  *  Created on: Feb 25, 2024
  *      Author: Imaduddien
@@ -7,20 +7,21 @@
 
 #include "QEI_X4.h"
 
-static void EncodeQEI(X4_QEI *self);
-static void ResetQEI(X4_QEI *self);
+static void Encode_X4_QEI(QEI *self);
+static void Encode_X2_QEI(QEI *self);
+static void ResetQEI(QEI *self);
 
-static void GetRPM(X4_QEI *self, uint32_t sample_t);
-static void GetRAD_S(X4_QEI *self, uint32_t sample_t);
-static void GetMTR_S(X4_QEI *self, uint32_t sample_t, float r);
+static void GetRPM(QEI *self, uint32_t sample_t);
+static void GetRAD_S(QEI *self, uint32_t sample_t);
+static void GetMTR_S(QEI *self, uint32_t sample_t, float r);
 
-static void GetDEG(X4_QEI *self);
-static void GetRAD(X4_QEI *self);
-static void GetMTR(X4_QEI *self, float r);
+static void GetDEG(QEI *self);
+static void GetRAD(QEI *self);
+static void GetMTR(QEI *self, float r);
 
-X4_QEI *Init_QEI(GPIO_TypeDef *PortA, GPIO_TypeDef *PortB, uint16_t PinA, uint16_t PinB, int32_t PPR){
-	X4_QEI *self;
-	self = (X4_QEI*)malloc(sizeof(X4_QEI));
+QEI *Init_X4_QEI(GPIO_TypeDef *PortA, GPIO_TypeDef *PortB, uint16_t PinA, uint16_t PinB, int32_t PPR){
+	QEI *self;
+	self = (QEI*)malloc(sizeof(QEI));
 
 	self->PortA = PortA;
 	self->PortB = PortB;
@@ -31,19 +32,20 @@ X4_QEI *Init_QEI(GPIO_TypeDef *PortA, GPIO_TypeDef *PortB, uint16_t PinA, uint16
 	self->pulse = 0;
 	self->PulseA = 0;
 	self->PulseB = 0;
-	self->curr = 0;
-	self->prev = 0;
-
-	self->GetPulse = EncodeQEI;
-	self->ResetQEI = ResetQEI;
 
 	self->RPM = self->RAD_S = self->MTR_S = 0;
 	self->RAD = self->DEG = self->METER = 0;
 
+	uint16_t temp_pulseA = (uint16_t)((self->PortA->IDR & self->PinA) == self->PinA);
+	uint16_t temp_pulseB = (uint16_t)((self->PortA->IDR & self->PinA) == self->PinA);
+	self->curr = (temp_pulseA << 1) | (temp_pulseB);
+	self->prev = self->curr;
+
+	self->GetPulse = Encode_X4_QEI;
+	self->ResetQEI = ResetQEI;
 	self->GetRAD_S = GetRAD_S;
 	self->GetRPM = GetRPM;
 	self->GetMTR_S = GetMTR_S;
-
 	self->GetRAD = GetRAD;
 	self->GetDEG = GetDEG;
 	self->GetMTR = GetMTR;
@@ -51,26 +53,75 @@ X4_QEI *Init_QEI(GPIO_TypeDef *PortA, GPIO_TypeDef *PortB, uint16_t PinA, uint16
 	return self;
 }
 
-static void EncodeQEI(X4_QEI *self){
+QEI *Init_X2_QEI(GPIO_TypeDef *EXTI_port, uint16_t EXTI_pin, GPIO_TypeDef *POLL_port, uint16_t POLL_pin, int32_t PPR){
+	QEI *self;
+	self = (QEI*)malloc(sizeof(QEI));
+
+	self->EXTI_Port = EXTI_port;
+	self->POLL_Port = POLL_port;
+	self->EXTI_Pin = EXTI_pin;
+	self->POLL_Pin = POLL_pin;
+
+	self->PPR = PPR;
+	self->pulse = 0;
+	self->PulseA = 0;
+	self->PulseB = 0;
+
+	self->RPM = self->RAD_S = self->MTR_S = 0;
+	self->RAD = self->DEG = self->METER = 0;
+
+	uint16_t temp_pulseA = (uint16_t)((self->EXTI_Port->IDR & self->EXTI_Pin) == self->EXTI_Pin);
+	uint16_t temp_pulseB = (uint16_t)((self->POLL_Port->IDR & self->POLL_Pin) == self->POLL_Pin);
+	self->curr = (temp_pulseA << 1) | (temp_pulseB);
+	self->prev = self->curr;
+
+	self->GetPulse = Encode_X2_QEI;
+	self->ResetQEI = ResetQEI;
+	self->GetRAD_S = GetRAD_S;
+	self->GetRPM = GetRPM;
+	self->GetMTR_S = GetMTR_S;
+	self->GetRAD = GetRAD;
+	self->GetDEG = GetDEG;
+	self->GetMTR = GetMTR;
+
+	return self;
+}
+
+static void Encode_X4_QEI(QEI *self){
 	int direction = 0; //1 = counter-clockwise, else clockwise
-	self->PulseA = (uint32_t)((self->PortA->IDR & self->PinA) == self->PinA);
-	self->PulseB = (uint32_t)((self->PortB->IDR & self->PinB) == self->PinB);
+	self->PulseA = (uint16_t)((self->PortA->IDR & self->PinA) == self->PinA);
+	self->PulseB = (uint16_t)((self->PortB->IDR & self->PinB) == self->PinB);
 
 	self->curr = (self->PulseA << 1) | (self->PulseB);
 
 	if(((self->curr ^ self->prev) != QEI_INVALID) && (self->curr != self->prev)){
 		direction = (self->prev & QEI_PREV_MSK) ^ ((self->curr & QEI_CURR_MSK) >> 1);
 		if(direction == 0) direction = -1;
+		else __NOP();
 		self->pulse -= direction;
 	}
 	self->prev = self->curr;
 }
 
-static void ResetQEI(X4_QEI *self){
+static void Encode_X2_QEI(QEI *self){
+	self->PulseA = (uint16_t)((self->PortA->IDR & self->PinA) == self->PinA);
+	self->PulseB = (uint16_t)((self->PortB->IDR & self->PinB) == self->PinB);
+	self->curr = (self->PulseA << 1) | (self->PulseB);
+
+	if(((self->prev == 0x3) && (self->curr == 0x0)) || ((self->prev == 0x0) && (self->curr == 0x3))){
+		self->pulse++;
+	}
+	else if(((self->prev == 0x2) && (self->curr == 0x1)) || ((self->prev == 0x1) && (self->curr == 0x2))){
+		self->pulse--;
+	}
+	self->prev = self->curr;
+}
+
+static void ResetQEI(QEI *self){
 	self->pulse = 0;
 }
 
-static void GetRPM(X4_QEI *self, uint32_t sample_t){
+static void GetRPM(QEI *self, uint32_t sample_t){
 	self->start_t = HAL_GetTick();
 	self->dt = self->start_t - self->prev_t;
 	if(self->dt >= sample_t){
@@ -80,7 +131,7 @@ static void GetRPM(X4_QEI *self, uint32_t sample_t){
 		self->prev_t = self->start_t;
 	}
 }
-static void GetRAD_S(X4_QEI *self, uint32_t sample_t){
+static void GetRAD_S(QEI *self, uint32_t sample_t){
 	self->start_t = HAL_GetTick();
 	self->dt = self->start_t - self->prev_t;
 	if(self->dt >= sample_t){
@@ -90,7 +141,7 @@ static void GetRAD_S(X4_QEI *self, uint32_t sample_t){
 		self->prev_t = self->start_t;
 	}
 }
-static void GetMTR_S(X4_QEI *self, uint32_t sample_t, float r){
+static void GetMTR_S(QEI *self, uint32_t sample_t, float r){
 	self->start_t = HAL_GetTick();
 	self->dt = self->start_t - self->prev_t;
 	if(self->dt >= sample_t){
@@ -101,12 +152,13 @@ static void GetMTR_S(X4_QEI *self, uint32_t sample_t, float r){
 	}
 }
 
-static void GetDEG(X4_QEI *self){
+static void GetDEG(QEI *self){
 	self->DEG = (self->pulse * 180.0f) / (4.0f * self->PPR);
 }
-static void GetRAD(X4_QEI *self){
+static void GetRAD(QEI *self){
 	self->RAD = (self->pulse * M_PI) / (4.0f * self->PPR);
 }
-static void GetMTR(X4_QEI *self, float r){
+static void GetMTR(QEI *self, float r){
 	self->METER = (self->pulse * M_PI * r * 2.0f) / (4.0f * self->PPR);
 }
+
